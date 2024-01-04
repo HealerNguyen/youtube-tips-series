d5352115296573f801245452e3d061df9eea4f42
Healer Nguyen <tuan.nguyen6@ntq-solution.com.vn>
2024-01-04 14:36:56 +0700

    wip: add js

---
+++ JS/main.js
@@ -0,0 +1,46 @@
+// let, const
+
+const myName = 'Healer Nguyen'
+myName = 'Healer'
+
+// const: ko the thay doi gia tri
+
+// let: co the thay doi gia tri
+
+let myCountry = 'USA'
+myCountry = 'VN'
+
+// String, Number, Boolean, null, undefined, Symbol
+// const myName = 'Healer';
+const age = 26
+const isSunny = true
+
+const stringConcat = 'My name is ' + myName + ', I am ' + age
+const stringLiteral = `My name is ${myName}, I am ${age}`
+
+console.log('stringConcat', stringConcat)
+console.log('stringLiteral', stringLiteral)
+
+//
+const string = 'Hello Guys'
+console.log('String length', string.length);
+
+// array: hold multiple values
+const fruits = ['apple', 'orange', 'pears']
+
+console.log(fruits);
+
+console.log(fruits[1]);
+
+fruits.push('mangos')
+
+fruits[4] = 'grapes'
+
+fruits.unshift('strawberries')
+
+fruits.pop()
+
+
+
+
+
